bf7256c22e5fad9f0058d310706e29e0e945d219
xc z <13521366337@163.com>
2026-03-24 13:19:53 +0800

    新增图像image16，添加折射材质类

--- image_render_base/material.h
+++ image_render_base/material.h
@@ -56,4 +56,26 @@ class metal : public material {
     float fuzz;
 };
 
+class dielectric : public material {
+  public:
+    dielectric(float refraction_index) : refraction_index(refraction_index) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        attenuation = color(1.0, 1.0, 1.0);
+        float ri = rec.front_face ? (1.0f/refraction_index) : refraction_index;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+
+  private:
+    // Refractive index in vacuum or air, or the ratio of the material's refractive index over
+    // the refractive index of the enclosing media
+    float refraction_index;
+};
+
 #endif
